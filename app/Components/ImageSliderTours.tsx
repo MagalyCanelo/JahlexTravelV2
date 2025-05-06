4883d6e6e9f1f61b2745bbdc460dbf8b3287ca05
JebGy <martinmunayco123@hotmail.com>
2025-05-05 19:40:07 -0500

    feat: ImageSliderTours funcionando

--- app/Components/ImageSliderTours.tsx
+++ app/Components/ImageSliderTours.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useKeenSlider } from "keen-slider/react";
+import type { KeenSliderInstance, KeenSliderHooks } from "keen-slider";
 import Image from "next/image";
 import React, { useState, useRef } from "react";
 
@@ -10,51 +13,41 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
   const touchStartX = useRef<number | null>(null);
   const touchEndX = useRef<number | null>(null);
 
-  const handleTouchStart = (e: React.TouchEvent) => {
-    touchStartX.current = e.touches[0].clientX;
-  };
-
-  const handleTouchMove = (e: React.TouchEvent) => {
-    touchEndX.current = e.touches[0].clientX;
-  };
-
-  const handleTouchEnd = () => {
-    if (touchStartX.current !== null && touchEndX.current !== null) {
-      const distance = touchStartX.current - touchEndX.current;
-      if (distance > 50) {
-        setCurrentImageIndex((prev) =>
-          prev === images.length - 1 ? 0 : prev + 1
-        );
-      } else if (distance < -50) {
-        setCurrentImageIndex((prev) =>
-          prev === 0 ? images.length - 1 : prev - 1
-        );
-      }
-    }
-    touchStartX.current = null;
-    touchEndX.current = null;
-  };
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    loop: true,
+    mode: "snap",
+    slides: { perView: 1, spacing: -15 },
+  });
 
   return (
-    <div
-      className="relative w-full h-full overflow-hidden rounded-lg"
-      onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
-      onTouchEnd={handleTouchEnd}
-    >
-      <Image
-        src={images[currentImageIndex]}
-        alt={`Imagen ${currentImageIndex + 1}`}
-        priority
-        width={500}
-        height={450}
-        className="w-full h-full object-cover"
-      />
+    <div className="relative w-full h-full overflow-hidden rounded-lg">
+      <div ref={sliderRef} className="keen-slider w-full overflow-hidden">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className="keen-slider__slide flex justify-center items-center p-0 gap-0"
+          >
+            <Image
+              src={image}
+              alt={`Imagen ${image + 1}`}
+              width={1000}
+              height={800}
+              className="w-full h-full object-cover"
+            />
+          </div>
+        ))}
+      </div>
+
       <div className="absolute bottom-2 right-3 flex gap-1 z-10">
         {images.map((_, index) => (
           <button
             key={index}
-            onClick={() => setCurrentImageIndex(index)}
+            onClick={() => {
+              if (instanceRef.current) {
+                instanceRef.current.moveToIdx(index);
+                setCurrentImageIndex(index);
+              }
+            }}
             className={`w-2.5 h-2.5 rounded-full transition-all ${
               index === currentImageIndex ? "bg-oliva-o" : "bg-gray-200"
             }`}
