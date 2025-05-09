06c04b7b892fd309cde4d924e30af129a7fb2888
JebGy <martinmunayco123@hotmail.com>
2025-05-09 00:17:42 -0500

    feat: Formularios
    - Agegué Firebase
    - Se diseñaron los formualrios acorde a interfaces
    - Se agregó la configuracion de Firebase
    - Recomiendo:
    - Instalar dependencias

---
+++ app/administrador/editar/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TurForm from "../components/TurForm";
+
+function page() {
+  return (
+    <div className="bg-white w-full h-full p-4 grid grid-rows-12 grid-cols-12 text-black">
+      <span className="row-span-2 col-span-12"></span>
+      <span className="row-span-2 col-span-1"></span>
+      <TurForm func="editar"/>
+    </div>
+  );
+}
+
+export default page;
--- app/administrador/page.tsx
+++ app/administrador/page.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import TurForm from './components/TurForm'
+import React from "react";
+import TurForm from "./components/TurForm";
+import ListaTours from "./components/ListaTours";
 
 function page() {
   return (
-    <div className='bg-white w-full h-screen flex flex-col items-center justify-center text-black'>
-      <TurForm/>
+    <div className="bg-white w-full h-screen p-4 grid grid-rows-12 grid-cols-12 text-black">
+      <span className="row-span-2 col-span-12"></span>
+      <span className="row-span-2 col-span-1"></span>
+      {/* <div className="row-span-8 col-span-10 bg-white rounded-lg shadow p-4">
+          <h2 className="text-xl font-bold">Agregar Sitio Turístico</h2>
+          <TurForm />
+        </div> */}
+        <ListaTours/>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
