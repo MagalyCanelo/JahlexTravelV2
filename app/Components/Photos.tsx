dd6b1c669f2a365ba986cd9fe68885047c44f095
JebGy <martinmunayco123@hotmail.com>
2025-05-09 11:03:33 -0500

    feat: Layout de galeria con grid creado

--- app/Components/Photos.tsx
+++ app/Components/Photos.tsx
@@ -44,24 +44,90 @@ const gridStyles = [
 
 const DestinationsMasonry: React.FC = () => {
   return (
-    <div className="bg-gray-50 grid grid-cols-3 auto-rows-[200px] md:auto-rows-[250px] lg:auto-rows-[300px] gap-4 p-4">
-      {destinations.map((dest, index) => (
-        <div
-          key={index}
-          className={`relative rounded-xl overflow-hidden ${
-            gridStyles[index % gridStyles.length]
-          }`}
-        >
-          <Image
-            src={dest.image}
-            alt={dest.title}
-            className="object-cover"
-            fill
-            sizes="(max-width: 768px) 100vw, 33vw"
-            priority={index < 2}
-          />
-        </div>
-      ))}
+    <div className="bg-gray-50 grid grid-cols-7 grid-rows-6 gap-4 p-4 h-screen">
+      <div className="col-span-4 row-span-2 bg-white shadow-lg rounded-lg ">
+        <Image
+          src={huacachina}
+          alt="Paracas"
+          className="w-full h-full object-cover rounded-lg"
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="col-span-1 row-span-2 bg-white shadow-lg rounded-lg">
+        <Image
+          src={rnp}
+          alt="Paracas"
+          className="w-full h-full object-cover rounded-lg"
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="col-span-2 row-span-3 bg-white shadow-lg rounded-lg">
+        <Image
+          src={ib}
+          alt="Paracas"
+          className="w-full h-full object-cover rounded-lg"
+          width={500}
+          height={500}
+        />
+      </div>
+
+      <div className="col-span-2 row-span-2 bg-white shadow-lg rounded-lg">
+        <Image
+          src={cusco}
+          alt="Paracas"
+          className="w-full h-full object-cover rounded-lg"
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="col-span-2 row-span-2 bg-white shadow-lg rounded-lg">
+        <Image
+          src={rnp}
+          alt="Paracas"
+          className="w-full h-full object-cover rounded-lg"
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="col-span-1 row-span-1 bg-white shadow-lg rounded-lg">
+      <Image
+          src={ib}
+          alt="Paracas"
+          className="w-full h-full object-cover rounded-lg"
+          width={500}
+          height={500}
+        />
+      </div>
+
+      <div className="col-span-2 row-span-3 bg-white shadow-lg rounded-lg">
+        <Image
+          src={cusco}
+          alt="Paracas"
+          className="w-full h-full object-cover rounded-lg"
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="col-span-1 row-span-3 bg-white shadow-lg rounded-lg">
+        <Image
+          src={rnp}
+          alt="Paracas"
+          className="w-full h-full object-cover rounded-lg"
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="col-span-4 row-span-2 bg-white shadow-lg rounded-lg">
+        <Image
+          src={ib}
+          alt="Paracas"
+          className="w-full h-full object-cover rounded-lg"
+          width={500}
+          height={500}
+        />
+      </div>
     </div>
   );
 };
