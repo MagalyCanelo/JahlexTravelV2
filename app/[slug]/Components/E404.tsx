b7f643efbbc391a512529f2075f6d2fc5f04697e
magaly <magalycanelo@gmail.com>
2025-06-07 19:05:44 -0500

    feat: diseño 404

---
+++ app/[slug]/Components/E404.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React from "react";
+import e404 from "@/public/404.png"; // Asegúrate de que la ruta sea correcta
+import Image from "next/image";
+import ActionButton from "@/app/Components/ActionButton";
+
+const ErrorPage: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center mb-24 mt-8 bg-stone-50">
+      {/* Imagen de error */}
+      <Image
+        src={e404.src}
+        alt="Logo de la empresa"
+        className="lg:w-120 w-60 xl:w-65"
+        height={1080}
+        width={1080}
+      />
+
+      {/* Mensaje de error */}
+      <h1 className="text-3xl font-bold text-gray-800 mb-3">
+        ¡Página errónea!
+      </h1>
+      <p className="text-lg text-gray-700 mb-6">
+        ¡Lo sentimos! ¡Esta página no está disponible!
+      </p>
+
+      {/* Botón para volver a la página principal */}
+      <div className="font-semibold text-xl">
+        <ActionButton
+          onClick={() => (window.location.href = "/")} // Redirige a la página principal
+          tipo="primary"
+          title="Volver a la página principal"
+          className="px-8"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+import E404 from "./Components/E404";
 
 function page() {
   return (
-    <div>page</div>
-  )
+    <>
+      <Header />
+      <E404 />
+      <Footer />
+    </>
+  );
 }
 
-export default page
+export default page;
