b301b4584e609aac62d95561797b7fc3350fd66c
JebGy <martinmunayco123@hotmail.com>
2025-06-13 16:32:29 -0500

    feat: 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,5 @@
-import Footer from "../Components/Footer";
-import Header from "../Components/Header";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
 import E404 from "./Components/E404";
 
 function page() {
