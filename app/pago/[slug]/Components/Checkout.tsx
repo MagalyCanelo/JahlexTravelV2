99101bb1919b81e17d8398eaa2d8ce650d7f7738
JebGy <martinmunayco123@hotmail.com>
2025-07-11 22:20:21 -0500

    feat: UI1

--- app/pago/[slug]/Components/Checkout.tsx
+++ app/pago/[slug]/Components/Checkout.tsx
@@ -1,12 +1,31 @@
+"use client";
 import CartNotice from "./CartNotice";
 import CheckoutForm from "./CheckoutForm";
 import ShoppingDetails from "./ShoppingDetails";
+import { ShoppingCarTour } from "@/app/interface/Tour";
+import { useUserStore } from "@/app/store/Usuario";
+import { getUserShoppingCar } from "@/service/FirebaseService";
+import { useEffect, useState } from "react";
 
 function CheckoutPage() {
+  const { user } = useUserStore();
+  const [userShoppingCar, setUserShoppingCar] = useState<ShoppingCarTour | null>(null);
+  const [tourName, setTourName] = useState("");
+
+  useEffect(() => {
+    getUserShoppingCar(user.id!).then((v) => {
+      setUserShoppingCar(v);
+      // Obtener el nombre del primer tour en el carrito
+      if (v && v.tours && v.tours.length > 0) {
+        setTourName(v.tours[0].title);
+      }
+    });
+  }, [user.id]);
+
   return (
     <div className="px-6 pt-2 pb-6 bg-stone-50">
       {/* Notificación superior */}
-      <CartNotice tourName="Caño Cristales River Trip" />
+      <CartNotice tourName={tourName} />
 
       {/* Contenido principal en dos columnas */}
       <div className="flex flex-col md:flex-row gap-5">
